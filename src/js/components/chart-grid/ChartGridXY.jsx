564f98904a01fd656887ea4dfb3057abb1ffcb87
golfecholima <george.levines@gmail.com>
2016-01-25 12:31:03 -0500

    adds sub logic to ChartGridXY

--- src/js/components/chart-grid/ChartGridXY.jsx
+++ src/js/components/chart-grid/ChartGridXY.jsx
@@ -95,7 +95,7 @@ var ChartGridXY = React.createClass({
 		var dimensions = clone(this.props.dimensions);
 
 		if (this.props.hasTitle) {
-			extraPadding.top = extraPadding.top + displayConfig.afterTitle;
+			extraPadding.top = extraPadding.top + displayConfig.afterTitle + displayConfig.afterSub;
 		}
 
 		/* Divide total width by number of grids, also subtracting the spade to be
