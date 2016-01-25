4b8a88d05cc576d6be75d3d2b0733b17f75d6fe4
golfecholima <george.levines@gmail.com>
2016-01-25 12:27:25 -0500

    adds sub properties and spacing for xy chart

--- src/js/charts/cb-xy/xy-config.js
+++ src/js/charts/cb-xy/xy-config.js
@@ -15,6 +15,7 @@ var ChartConfig = require("../ChartConfig");
 * @property {Nem|number} labelRowHeight - Vert distance btwn rows of labels
 * items with colors the appropriate indexed CSS class
 * @property {Nem|number} afterTitle - Distance btwn top of title and top of legend or chart
+* @property {Nem|number} afterSub - Distance btwn top of sub and top of legend or chart
 * @property {Nem|number} afterLegend - Distance btwn top of legend and top of chart
 * @property {Nem|number} blockerRectOffset - Distance btwn text of axis and its background blocker
 * @property {Nem|number} columnPaddingCoefficient - Distance relative to
@@ -34,8 +35,9 @@ var display = {
 	labelXMargin: "0.6em",
 	labelTextMargin: "0.3em",
 	labelRowHeight: "1.2em",
-	afterTitle: "1.6em",
-	afterLegend: "1.6em",
+	afterTitle: "3em",
+	afterSub: "0em",
+	afterLegend: "1em",
 	blockerRectOffset: "0.3em",
 	columnPaddingCoefficient: 0.3,
 	minPaddingOuter: "1em",
