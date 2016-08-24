ed68c6df915460f3584a7dbcb76407c2ae04f1f4
jiayou <jyou@c02qxbcxg8wn.aaas.org>
2016-08-24 17:11:34 -0400

    change x-axis tick size to 0

--- src/js/config/chart-style.js
+++ src/js/config/chart-style.js
@@ -16,7 +16,7 @@
  */
 var chart_style = {
 	overtick_top: "0.8em",
-	overtick_bottom: 10,
+	overtick_bottom: 0,
 	numColors: 12,
 	xOverTick: "1em", // horizontal the distance between the yAxes and xAxis
 	creditMargin: "0.6em"
