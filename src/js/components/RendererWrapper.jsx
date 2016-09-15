3de2b54394057bba3b56c1c8b4376dd84348e15a
jiayou <jyou@c02qxbcxg8wn.aaas.org>
2016-09-15 13:24:02 -0400

    axis labels are centered for all chart sizes & title heights

--- src/js/components/RendererWrapper.jsx
+++ src/js/components/RendererWrapper.jsx
@@ -165,8 +165,6 @@ var RendererWrapper = React.createClass({
 	_handleSvgUpdate: function(k, v) {
 		var newSetting = {};
 		newSetting[k] = v;
-		console.log(k, v);
-		console.log(newSetting);
 		this.setState(update(this.state, { $merge: newSetting }));
 	},
 
@@ -245,8 +243,6 @@ var RendererWrapper = React.createClass({
 		} else {
 			metadata = this.props.model.metadata;
 		}
-
-		console.log(this.state);
 
 		var margin = this.state.chartConfig.display.margin;
 		var metadataSvg = [];
